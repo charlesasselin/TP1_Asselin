5fe2790aae6cb402b09959a43b132f217d5c56c3
Charles Asselin <charles@asselin.xyz>
2020-03-15 11:47:55 -0400

    Premier commit, ceci est un test en soi, et comporte les premières modifications du projet TP1.

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import argparse
 
+
+def analyser_commande():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '-h', '--help', help = 'show this message and exit'
+    )
+
+    parser.add_argument(
+        '-l', '--lister', help= 'Lister les identifiants de vos 20 dernieres parties'
+    )
+
+    return parser.parse_args()
