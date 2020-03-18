29cae6bf854e23be04e75999634bae4e99d1d1e5
charlesasselin <charles@asselin.xyz>
2020-03-18 19:52:20 -0400

    Création de la première fonction: 'lister_parties' qui va retourner le nombre de parties jouées selon l'idul. Si la requête ne se déroule pas normalement, le code d'erreur RunTimeError s'affiche.

--- api.py
+++ api.py
@@ -1,12 +1,13 @@
 import requests
 
-url_base = 'https://python.gel.ulaval.ca/quoridor/api'
+def lister_parties(idul):
 
-rep = requests.get(f'{url_base}/lister/', params={'idul': 'xxxxx'})
-if rep.status_code == 200:
-    # la requête s'est déroulée normalement; décoder le JSON
-    rep = rep.json()
-    print(rep)
+    url_base = 'https://python.gel.ulaval.ca/quoridor/api'
 
-else:
-    print(f"Le GET sur {url_base + 'lister'} a produit le code d'erreur {rep.status_code}.")
+    rep = requests.get(f'{url_base}/lister/', params={'idul': idul})
+    if rep.status_code == 200:
+        # la requête s'est déroulée normalement; décoder le JSON
+        rep = rep.json()
+        return rep['parties']
+    else:
+        raise RuntimeError(f"Le GET sur {url_base + 'lister'} a produit le code d'erreur {rep.status_code}.")
