7af2c1c83592bc1d1833245d4bc554c06dc93b0a
charlesasselin <charles@asselin.xyz>
2020-03-18 16:59:30 -0400

    J'ai mis à jour mon environnement qui n'était pas en python 3.7 encore (il était en 2.7 avant). J'ai aussi fait la fonction qui transforme les murs horizontaux. Début de la boucle pour laquelle la position des joueurs est identifiable.

--- quoridor.py
+++ quoridor.py
@@ -13,27 +13,48 @@ def afficher_damier_ascii(dico_etat_jeu):
 
     #---------- MODIFIER DICO ----------
 
-    def transformJoueurs():
+    def transformJoueurs(dico_etat_jeu):
         listJoueurs = []
         for joueur in dico_etat_jeu['joueurs']:
             listJoueurs.append([(2*joueur['pos'][0])-1, (2*joueur['pos'][1])-1])
+        return listJoueurs
 
-    def transformMurVert():
-       listTransformedVert = []
+    def transformMurVert(dico_etat_jeu):
+        listTransformedVert = []
         for murV in dico_etat_jeu['murs']['verticaux']:
             transformedMurV = [(2*murV[0])-1, (2*murV[1])-1]
 
-            extendWall = []
+            extendWallVert = []
             for i in range(3):
-                extendWall.append([transformedMurV[0]-1, transformedMurV[1] + i])
-            listTransformedVert.extend(extendWall)
+                extendWallVert.append([transformedMurV[0]-1, transformedMurV[1] + i])
+            listTransformedVert.extend(extendWallVert)
+
+        return listTransformedVert
 
 
+    def transformMurHori(dico_etat_jeu):
+        listTransformedHori = []
+        for murH in dico_etat_jeu['murs']['horizontaux']:
+            transformedMurH = [(2*murH[0])-1, (2*murH[1])-1]
 
+            extendWallHori = []
+            for i in range(7):
+                extendWallHori.append([transformedMurH[0] + i, transformedMurH[0]-1])
+            listTransformedHori.extend(extendWallHori)
+        return listTransformedHori
     #---------- BOUCLES GRILLE ----------
 
+    joueurs = transformJoueurs(dico_etat_jeu)
+    mursVerticaux = transformMurVert(dico_etat_jeu)
+    mursHorizontaux = transformMurHori(dico_etat_jeu)
     for i in range(17, 1, -1):
         for j in range(1, 10):
+            if [i, j] in joueurs:
+                print(joueurs.index([i, j])+1)
+
+
+
+
 
     #il faudra iterer selon les valeurs du dictionnaire ici
 
