97342a2c7fd7db78d0b331f5d1cd0304705ab7f5
charlesasselin <charles@asselin.xyz>
2020-03-18 13:11:48 -0400

    Première étape pour la grille de jeu.

---
+++ quoridor.py
@@ -0,0 +1,24 @@
+def afficher_damier_ascii(dico_etat_jeu):
+
+    #---------- ASCII START ---------
+    patron = ''
+    ligneStart = ' ----------------------------------- \n'
+    # --------- OPTIONS ----------
+
+    name1 = ' 1 '
+    name2 = ' 2 '
+    empty = ' . '
+    vertCorridor = '-------'
+    horiCorridor = '| '
+
+    #---------- BOUCLES GRILLE ----------
+
+    for i in range(10, 1, -1):
+        for j in range(1, 10):
+            #il faudra iterer selon les valeurs du dictionnaire ici
+
+    #---------- ASCII END ----------
+    ligneX = '--|--------------------------------- \n'
+    ligneXValues = '  | 1   2   3   4   5   6   7   8   9  '
+
+
