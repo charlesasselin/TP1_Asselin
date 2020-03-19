518a1cc0f8522586222ba365919ec5fcb824f711
charlesasselin <charles@asselin.xyz>
2020-03-18 20:12:24 -0400

    Ajout de la deuxième fonction: 'initialiser_partie'

--- api.py
+++ api.py
@@ -11,3 +11,11 @@ def lister_parties(idul):
         return rep['parties']
     else:
         raise RuntimeError(f"Le GET sur {url_base + 'lister'} a produit le code d'erreur {rep.status_code}.")
+
+def initialiser_partie(idul):
+    rep = requests.post(f'{url_base}/initialiser/', data={'idul': idul})
+    dico = rep.json()
+    if dico.get(['message']):
+        raise RuntimeError(f"Le GET sur {url_base + 'lister'} a produit le code d'erreur {rep.status_code}.")
+    else:
+        return dico['id'], dico['état']
