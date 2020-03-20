c4264862a833415483c51e4d31ba07c6c350cc11
charlesasselin <charles@asselin.xyz>
2020-03-20 17:26:01 -0400

    Ajout de la troisième et dernière fonction du module api: 'jouer'.

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 import requests
 
+url_base = 'https://python.gel.ulaval.ca/quoridor/api'
+
 def lister_parties(idul):
 
-    url_base = 'https://python.gel.ulaval.ca/quoridor/api'
+
 
     rep = requests.get(f'{url_base}/lister/', params={'idul': idul})
     if rep.status_code == 200:
@@ -16,6 +18,16 @@ def initialiser_partie(idul):
     rep = requests.post(f'{url_base}/initialiser/', data={'idul': idul})
     dico = rep.json()
     if dico.get(['message']):
-        raise RuntimeError(f"Le GET sur {url_base + 'lister'} a produit le code d'erreur {rep.status_code}.")
+        raise RuntimeError(f"Le GET sur {url_base + 'initialiser'} a produit le code d'erreur {rep.status_code}.")
     else:
         return dico['id'], dico['état']
+
+def jouer_coup(id_partie, type_coup, position):
+    rep = requests.post(f'{url_base}/jouer/')
+    dico = rep.json()
+    if dico.get(['gagnant']):
+        raise StopIteration(f'Le gagnant est {dico["gagnant"]}')
+    elif dico.get(['message']):
+        raise RuntimeError(f"Le GET sur {url_base + 'jouer'} a produit le code d'erreur {rep.status_code}.")
+    else:
+        return dico['état']
